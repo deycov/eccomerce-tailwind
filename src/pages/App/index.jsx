7f752bc8cde32b61b924b47d0bccda757c92f8d9
Deycov <53343872+adrianA-R@users.noreply.github.com>
2024-01-11 23:17:30 -0400

    subida doble

--- src/pages/App/index.jsx
+++ src/pages/App/index.jsx
@@ -1,20 +1,33 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import Navbar from "../../components/Nav";
 import "./App.css";
 
-const appRouter = createBrowserRouter([
-  {
-    path: "/",
-    element: <> Home </>,
-  },
-]);
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
 
-function App() {
+  return routes;
+};
+
+const App = () => {
   return (
-    <>
-      <RouterProvider router={appRouter} />
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+      <Navbar />
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
